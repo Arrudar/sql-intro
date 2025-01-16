3dafa82308c6ce05856b0830cc330f50a10f99fb
Pedro Rico <pedro.rico@kellogg.northwestern.edu>
2025-01-16 02:59:01 +0000

    Solved exercise 4.1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,21 @@
 -- How many lifetime hits does Barry Bonds have?
 
+--Verify The player I need to check
+
+select id, first_name, last_name
+from players
+where first_name = "Barry"
+and last_name = "Bonds";
+
+____
+
+select SUM (hits)
+from players
+INNER JOIN stats ON players.id = stats.player_id
+where first_name = "Barry"
+and last_name = "Bonds";
+
+
 -- Expected result:
 -- 2935
 
